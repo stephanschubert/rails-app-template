73247c0ca136075cf6bbb17feff03ed784e76de9
jazen <stephan@frozencherry.de>
2009-03-08 19:12:45 +0100

    Fixed syntax error

--- template.rb
+++ template.rb
@@ -12,7 +12,7 @@ plugin "rspec",
   :git => "git://github.com/dchelimsky/rspec.git", :submodule => true
 plugin "rspec-rails",
   :git => "git://github.com/dchelimsky/rspec-rails.git", :submodule => true
-plugin => "factory_girl", 
+plugin "factory_girl", 
   :git => "git://github.com/thoughtbot/factory_girl.git", :submodule => true
 
 # Generators
