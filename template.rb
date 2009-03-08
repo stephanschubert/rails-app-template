1da9d5ff9189a6cd0aac862bbaccbdfbfcf1673f
jazen <stephan@frozencherry.de>
2009-03-08 19:10:00 +0100

    Add Rspec/Rails as submoduled plugin. Add thoughtbot's factory_girl as submoduled plugin.

--- template.rb
+++ template.rb
@@ -1,7 +1,27 @@
-# Setup git repository
+# Setup a fresh Rails application with git
+# Use my sake task git:hub:rails:new_app for example.
 
-git :init
+# Delete unnecessary files
+
+run "rm README"
+run "rm public/index.html"
+
+# Install submoduled plugins
+
+plugin "rspec", 
+  :git => "git://github.com/dchelimsky/rspec.git", :submodule => true
+plugin "rspec-rails",
+  :git => "git://github.com/dchelimsky/rspec-rails.git", :submodule => true
+plugin => "factory_girl", 
+  :git => "git://github.com/thoughtbot/factory_girl.git, :submodule => true
+
+# Generators
+
+generate("rspec")
+
+# Commit to git
+
 git :add => "."
-git :commit => "-a -m 'First commit'"
+git :commit => "-a -m 'Applied application template from github.com/jazen/rails-app-template'"
 
 # More to come.
