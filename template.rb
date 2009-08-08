6abeedd6f666273329fedda421cdd9cf8dd095dd
jazen <stephan@frozencherry.de>
2009-08-08 18:13:13 +0200

    Small fix

--- template.rb
+++ template.rb
@@ -70,7 +70,7 @@ template do
   git :commit => "-a -m 'Initial commit'"
 
   # Set up session store initializer
-  initializer 'session_store.rb', <<-EOS.gsub(/^ /, '')
+  initializer 'session_store.rb', <<-EOS.gsub(/^  /, '')
   ActionController::Base.session = { :session_key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }
   ActionController::Base.session_store = :active_record_store
   EOS
